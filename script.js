be9badba32db66b580e39fa6f966bed4c64ee98a
sanapalavamsikrishna45 <vamsisanapal2@gmail.com>
2025-09-29 20:34:27 +0530

    second commit

---
+++ script.js
@@ -0,0 +1,23 @@
+
+const apiKey = "3c76f9dd4e208036f242c08f25ea431b";
+
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+
+async function checkweather(city){
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    var data = await response.json();
+    console.log(data);
+
+    document.querySelector(".city").innerHTML=data.name;
+    document.querySelector(".temp").innerHTML=Math.round(data.main.temp) +"°C";
+    document.querySelector(".humidity").innerHTML=data.main.humidity +"%";
+    document.querySelector(".wind").innerHTML=data.wind.speed + " Km/h";
+}
+
+searchBtn.addEventListener("click", ()=>{
+    checkweather(searchBox.value)
+})
+checkweather();
